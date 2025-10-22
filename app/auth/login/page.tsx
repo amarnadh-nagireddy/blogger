3780a34e7bfe0cde3a3921aa3db151aa2795e5c5
Amarnadh Nagireddy <amarnadh.nagiredy@outlook.com>
2025-10-22 16:39:18 +0530

    latest for production

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -30,6 +30,7 @@ export default function LoginPage() {
             
             const response = await axios.post("/api/users/login", user);
             console.log("Login success", response.data);
+            toast.success("Login successful");
             router.push("/admin");
 
         }catch (error: unknown) {
